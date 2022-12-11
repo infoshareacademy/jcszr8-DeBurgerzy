9cd18f283d23ae034083e0e4f9d2ecf0d0def36e
Maciej Dusza <md.bimsolutions@gmail.com>
2022-12-11 20:55:15 +0100

    Console project changed name. First Model

--- ParcelDistributionCenter/ParcelDistributionCenter.Model/Class1.cs
+++ ParcelDistributionCenter/ParcelDistributionCenter.Model/Class1.cs
@@ -5,3 +5,23 @@
 
     }
 }
+
+/* Model:
+ * 
+ * Parcel ( int Id, "enum" Status, "enum" size, int CourierId,Client Sender,Client Recipient, Address Current)
+ * 
+ * Client( int Id, string Name, strin Surname, Address Address)
+ * 
+ * ParcelLocker( int Id, "enum" status, ParcelLockerPlace ParcelLockerPlace, Address Address )
+ * 
+ * ParcelLockerPlace ( int L, int S, int M )  // do przemyślenia czy zostawiamy to jako oddzielną klasę
+ *  
+ * DeliveryCenter( int Id,)
+ * 
+ * Courier( int Id, )
+ * 
+ * Address ( )
+ * 
+ * 
+
+*/
