ce8b9eba9ca18572ca1f5ec00dbcd3805400234d
Maciej Dusza <md.bimsolutions@gmail.com>
2023-01-06 21:37:09 +0100

    Komentarze do wczytywania danych

--- ParcelDistributionCenter/ParcelDistributionCenter.Concole/Program.cs
+++ ParcelDistributionCenter/ParcelDistributionCenter.Concole/Program.cs
@@ -11,7 +11,7 @@ namespace ParcelDistributionCenter.ConsoleUI
 
             // string ParcelUser = File.ReadAllText(@"D:\packages.json");//odczytanie json z pliku. Ścieżkę dodana na sztywno.
             //plik Json umieszczony w projekcie :jcszr8-DeBurgerzy\ParcelDistributionCenter\ParcelDistributionCenter.Concole\bin\Debug\net6.0
-            string ParcelUser = File.ReadAllText("packages.json");
+            string ParcelUser = File.ReadAllText("..\\..\\..\\..\\ParcelDistributionCenter.Model\\json\\packages.json");
             List<Parcel> UserList1 = JsonConvert.DeserializeObject<List<Parcel>>(ParcelUser);//wpisanie json do listy
 
             foreach (var item in UserList1)//testowe wyswietlanie listy użytkowników itp.
@@ -21,9 +21,11 @@ namespace ParcelDistributionCenter.ConsoleUI
                 // Console.WriteLine($"Adres paczkomatu: {item.Delivery_machine_id}");
                 // Console.WriteLine($"Adres dostawy: {item.Delivery_address}");
                 //Console.WriteLine($"Data rejestracji: {item.Registered}");
+                Console.WriteLine($"Data rejestracji: {item.Registered}");
                  Console.WriteLine("");
             }
 
+            /*
             UserList1[0].Size = ParcelSize.Big;
             UserList1[1].Size = ParcelSize.Big;
             UserList1[2].Size = ParcelSize.Big;
@@ -32,6 +34,7 @@ namespace ParcelDistributionCenter.ConsoleUI
             {
                 Console.WriteLine($"Rozmiar paczki: {item.Size}");
             }
+            */
         }
     }
 }
