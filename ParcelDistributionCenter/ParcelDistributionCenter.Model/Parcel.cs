c515259c388d18062121879780f2d0a26a4ce737
Maciej Dusza <md.bimsolutions@gmail.com>
2023-01-08 20:26:07 +0100

    Task7 Wyszukiwanie paczki oraz nowy json

--- ParcelDistributionCenter/ParcelDistributionCenter.Model/Parcel.cs
+++ ParcelDistributionCenter/ParcelDistributionCenter.Model/Parcel.cs
@@ -4,7 +4,7 @@ namespace ParcelDistributionCenter.Model
 {
     public class Parcel
     {
-        public uint Package_number { get; init; }
+        public string Package_number { get; init; }
         public ParcelSize Size { get; init; }
         public string Sender_email { get; set; }
         public string Send_parcel_id { get; set; }
@@ -12,13 +12,13 @@ namespace ParcelDistributionCenter.Model
         public string Delivery_parcel_id { get; set; }
         public Status Status { get; set; }
         public string Current_location_id { get; set; }
-        public string Courier_id { get; set; }
+        public string? Courier_id { get; set; }
         public DateTime Registered { get; set; }
-        public DateTime Deliver_date { get; set; }
+        public DateTime? Deliver_date { get; set; }
         public DateTime? Expire_date { get; set; }
 
 
-        public Parcel(uint package_number, ParcelSize size, string sender_email, string send_parcel_id, string recipient_email, string delivery_parcel_id) // konstruktor przy starcie programu - wczytywanie z bazy
+        public Parcel(string package_number, ParcelSize size, string sender_email, string send_parcel_id, string recipient_email, string delivery_parcel_id, Status status, string current_location_id, string? courier_id, DateTime registered) // konstruktor przy starcie programu - wczytywanie z bazy
         {
             Package_number = package_number;
             Size = size;
@@ -26,16 +26,17 @@ namespace ParcelDistributionCenter.Model
             Send_parcel_id = send_parcel_id;
             Recipient_email = recipient_email;
             Delivery_parcel_id = delivery_parcel_id;
-            Status = (Status)0;
-            Current_location_id = "Sender";
-            Courier_id = "0";
-            Registered = DateTime.Now;
-            Expire_date = Registered.AddDays(2);
+            Status = status;
+            Current_location_id = current_location_id;
+            Courier_id = courier_id;
+            Registered = registered;
+            Deliver_date=null; 
+            Expire_date = null;
         }
 
-        public Parcel(ParcelSize size, string sender_email, string send_parcel_id, string recipient_email, string delivery_parcel_id) // konstruktor przy wprowadzaniu paczki
+       /* public Parcel(ParcelSize size, string sender_email, string send_parcel_id, string recipient_email, string delivery_parcel_id) // konstruktor przy wprowadzaniu paczki
         {
-            Package_number = 1; // Do opracowania algorytm nadawania numerów paczek;
+            Package_number = "1"; // Do opracowania algorytm nadawania numerów paczek;
             Size = size;
             Sender_email = sender_email;
             Send_parcel_id = send_parcel_id;
@@ -47,6 +48,9 @@ namespace ParcelDistributionCenter.Model
             Registered = DateTime.Now;
             Expire_date = Registered.AddDays(2);
         }
+
+        */
+
 
         //public string Delivery_address { get; set; } -  tego parametru nie rozumiem - dostarczamy do paczkomatu więc wszystko jest już w Parcel_id.
         // public string Sender_name { get; set; } - do klienta
