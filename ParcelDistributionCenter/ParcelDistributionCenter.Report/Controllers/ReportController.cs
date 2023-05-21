248f83015628e8f3ecbc66824ba3037d7f3a66f8
MichalPietrzak1 <116110544+MichalPietrzak1@users.noreply.github.com>
2023-05-21 16:41:48 +0200

    API project added + automaper added + default structure added

---
+++ ParcelDistributionCenter/ParcelDistributionCenter.Report/Controllers/ReportController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ParcelDistributionCenter.Report.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReportController : ControllerBase
+    {
+    }
+}
---
+++ ParcelDistributionCenter/ParcelDistributionCenter.Report/Program.cs
@@ -0,0 +1,31 @@
+using AutoMapper;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program));
+
+var app = builder.Build();
+
+IMapper mapper = (IMapper)app.Services.GetRequiredService(typeof(IMapper));
+mapper.ConfigurationProvider.AssertConfigurationIsValid();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
